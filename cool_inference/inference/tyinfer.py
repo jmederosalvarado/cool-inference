7e1bd441944fd1776d63447cf80271178c9b3efd
Rodrigo <rodrigo.garcia@estudiantes.matcom.uh.cu>
2020-11-27 03:10:14 -0500

    Changes in tyinfer fix BagCollector fix dispatch add restriction checker to id node

--- cool_inference/inference/tyinfer.py
+++ cool_inference/inference/tyinfer.py
@@ -29,7 +29,6 @@ from cool_inference.ast import (
     IntExp,
     StringExp,
     BoolExp,
-    Binary,
     Plus,
     Minus,
     Mult,
@@ -41,12 +40,12 @@ from cool_inference.ast import (
 
 
 class BagsCollector:
-    def __init__(self, context, errors=[]):
+    def __init__(self, context, errors=[]):  # noqa: F811
         self.context = context
         self.errors = errors
 
     @visitor.on("node")
-    def visit(self, node, bags):
+    def visit(self, node, bags):  # noqa: F811
         pass
 
     @visitor.when(Program)
@@ -65,43 +64,58 @@ class BagsCollector:
 
         tybags.define_variable("self", [self.current_type.name])
 
-        # TODO: hacer esto en el visit de FuncDecl
         for method in self.current_type.methods:
             if method.return_type == AutoType():
-                types = set(self.context.types.keys())
+                types = list(self.context.types.keys())
             else:
                 types = [method.return_type.name]
+
             tybags.define_variable(method.name, types)
 
-        # TODO: hacer esto en el visit de AttrDecl
         for attr in self.current_type.attributes:
             if attr.type == AutoType():
-                types = set(self.context.types.keys())
+                types = list(self.context.types.keys())
             else:
                 types = [attr.type.name]
             tybags.define_variable(attr.name, types)
 
         for feat in node.feature_list:
             self.visit(feat, tybags)
-
         return tybags
+
+    @visitor.when(AttrDecl)
+    def visit(self, node, tybags):  # noqa: F811
+        return
 
     @visitor.when(FuncDecl)
     def visit(self, node, tybags):  # noqa: F811
         self.current_method = self.current_type.get_method(node.id)
         method_tybags = tybags.create_child(node)
+        self.current_method.tybags = method_tybags
 
         for pname, ptype in zip(
             self.current_method.param_names, self.current_method.param_types
         ):
             if ptype == AutoType():
-                method_tybags.define_variable(pname, set(self.context.types.keys()))
+                method_tybags.define_variable(pname, list(self.context.types.keys()))
             else:
                 method_tybags.define_variable(pname, [ptype.name])
 
         self.visit(node.body, method_tybags)
 
         self.current_method = None
+
+    @visitor.when(Assign)
+    def visit(self, node, tybags):  # noqa: F811
+        return
+
+    @visitor.when(Dispatch)
+    def visit(self, node, tybags):  # noqa: F811
+        return
+
+    @visitor.when(StaticDispatch)
+    def visit(self, node, tybags):  # noqa: F811
+        return
 
     @visitor.when(LetIn)
     def visit(self, node, tybags):  # noqa: F811
@@ -112,7 +126,7 @@ class BagsCollector:
             typex = self.context.get_type(_type)
 
             if typex == AutoType():
-                let_tybags.define_variable(idx, set(self.context.types.keys()))
+                let_tybags.define_variable(idx, list(self.context.types.keys()))
             else:
                 let_tybags.define_variable(idx, [typex.name])
 
@@ -130,7 +144,7 @@ class BagsCollector:
             typex = self.context.get_type(typex)
 
             if typex == AutoType():
-                new_tybags.define_variable(idx, set(self.context.types.keys()))
+                new_tybags.define_variable(idx, list(self.context.types.keys()))
             else:
                 new_tybags.define_variable(idx, [typex.name])
 
@@ -148,7 +162,7 @@ class BagsCollector:
     # TODO
     @visitor.when(Tilde)
     def visit(self, node, tybags):  # noqa: F811
-        pass
+        return
 
     @visitor.when(IsVoid)
     def visit(self, node, tybags):  # noqa: F811
@@ -158,8 +172,46 @@ class BagsCollector:
     def visit(self, node, tybags):  # noqa: F811
         self.visit(node.exp, tybags)
 
-    @visitor.when(Binary)
+    def arith(self, node, tybags):  # noqa: F811
+
+        self.visit(node.left, tybags)
+        self.visit(node.right, tybags)
+
+    @visitor.when(Plus)
     def visit(self, node, tybags):  # noqa: F811
+        self.arith(node, tybags)
+
+    @visitor.when(Minus)
+    def visit(self, node, tybags):  # noqa: F811
+        self.arith(node, tybags)
+
+    @visitor.when(Div)
+    def visit(self, node, tybags):  # noqa: F811
+        self.arith(node, tybags)
+
+    @visitor.when(Mult)
+    def visit(self, node, tybags):  # noqa: F811
+        self.arith(node, tybags)
+
+    def comp(self, node, tybags):  # noqa: F811
+        self.visit(node.left, tybags)
+        self.visit(node.right, tybags)
+
+    @visitor.when(Leq)
+    def visit(self, node, tybags):  # noqa: F811
+        self.comp(node, tybags)
+
+    @visitor.when(Eq)
+    def visit(self, node, tybags):  # noqa: F811
+        self.comp(node, tybags)
+
+    @visitor.when(Le)
+    def visit(self, node, tybags):  # noqa: F811
+        self.comp(node, tybags)
+
+    @visitor.when(WhileLoop)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
         self.visit(node.left, tybags)
         self.visit(node.right, tybags)
 
@@ -168,6 +220,26 @@ class BagsCollector:
         self.visit(node.first, tybags)
         self.visit(node.second, tybags)
         self.visit(node.third, tybags)
+
+    @visitor.when(StringExp)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
+
+    @visitor.when(BoolExp)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
+
+    @visitor.when(IntExp)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
+
+    @visitor.when(IdExp)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
+
+    @visitor.when(NewType)
+    def visit(self, node, tybags):  # noqa: F811
+        pass
 
 
 class BagsReducer:
@@ -223,13 +295,7 @@ class BagsReducer:
 
     @visitor.when(Assign)
     def visit(self, node, tybags, restriction):  # noqa: F811
-        types = self.visit(
-            node.value,
-            tybags,
-            []
-            if tybags.find_variable(node.id) is None
-            else tybags.find_variable(node.id),
-        )
+        types = self.visit(node.value, tybags, [])
 
         tybags.reduce_bag(node, types)
         return tybags.find_variable(node.id)
@@ -237,22 +303,44 @@ class BagsReducer:
     # TODO fix dispatch. Find an elegant way to do it.
     @visitor.when(Dispatch)
     def visit(self, node, tybags, restriction):  # noqa: F811
-        exp_type = self.context.types[list(self.visit(node.exp, tybags, []))[0]]
+        exp_types = self.visit(node.exp, tybags, [])
+        if len(exp_types) > 1:
+            types_whith_method = []
+            return_types = []
+            for key, value in self.context.types.items():
+                for method in value.methods:
+                    if (
+                        key in exp_types
+                        and method.name == node.id
+                        and len(method.param_names) == len(node.exp_list)
+                    ):
+                        types_whith_method.append(key)
+                        return_types.append(method.return_type.name)
+                        break
 
-        for arg in node.exp_list:
-            arg_types = self.visit(arg, tybags, [])
-            tybags.reduce_bag(arg, arg_types)
+            if len(types_whith_method) == 0:
+                error = f"""
+                There is not possible type of expression that
+                have {node.id} method with {len(node.exp_list)} params
+                """
+                if error not in self.errors:
+                    self.errors.append(error)
+                return set([self.context.types["ERROR"]])
 
-        while tybags.parent is not None:
-            tybags = tybags.parent
+            tybags.reduce_bag(node.exp, types_whith_method)
+            return set(return_types)
 
-        for _, ty in tybags.children.items():
+        elif len(exp_types) == 1:
+            exp_type = self.context.types[list(exp_types)[0]]
+            method = exp_type.get_method(node.id)
 
-            if list(ty.vars["self"])[0] == exp_type.name:
-                tybags = ty
-                break
+            function_ty = method.tybags
 
-        return tybags.find_variable(node.id)
+            for arg, param in zip(node.exp_list, method.param_names):
+                arg_types = self.visit(arg, tybags, function_ty.vars[param])
+                function_ty.reduce_bag(None, arg_types, name=param)
+
+            return function_ty.find_variable(node.id)
 
     # TODO
     @visitor.when(StaticDispatch)
@@ -406,6 +494,9 @@ class BagsReducer:
 
     @visitor.when(IdExp)
     def visit(self, node, tybags, restriction):  # noqa: F811
+        if len(restriction) > 0:
+            tybags.reduce_bag(node, restriction)
+
         return tybags.find_variable(node.id)
 
     @visitor.when(NewType)
